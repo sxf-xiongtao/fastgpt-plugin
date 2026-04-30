b73bcc1a31e0c216cba13712a8e7b8782ba20325
hello_strong <1145607886@qq.com>
2026-04-30 15:49:52 +0800

    feat: 内置模板国际化

--- modules/workflow/schemas/routes.ts
+++ modules/workflow/schemas/routes.ts
@@ -1,4 +1,4 @@
-import { createRoute } from '@hono/zod-openapi';
+import { createRoute, z } from '@hono/zod-openapi';
 import { createResponseSchema, ErrorResponseSchema } from '@/utils/http';
 import { TemplateListSchema } from './common';
 
@@ -8,7 +8,16 @@ export const listWorkflowsRoute = createRoute({
   path: '/list',
   tags: ['Workflows'],
   summary: 'List workflow templates',
-  description: 'Get a list of all available workflow templates',
+  description: 'Get a list of all available workflow templates, optionally filtered by locale',
+  request: {
+    query: z.object({
+      locale: z
+        .enum(['zh-CN', 'zh-Hant', 'en'])
+        .optional()
+        .default('zh-CN')
+        .describe('The locale to filter templates by')
+    })
+  },
   responses: {
     200: {
       description: 'List of workflow templates',
